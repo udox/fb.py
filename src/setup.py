a87e00c3e4aae5a7c3d8400e254e205827ef6c4d
Emre Yılmaz <email@emreyilmaz.me>
2010-07-07 17:40:05 +0300

    refactored directory structure

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='fbpy',
+      version='1.0',
+      description='python sdk for facebook apis',
+      author='Emre Yilmaz',
+      author_email='mail@emreyilmaz.me',
+      url='http://www.python.org/sigs/distutils-sig/',
+      packages = ['fbpy'],
+     )
+
