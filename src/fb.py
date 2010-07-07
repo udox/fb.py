aa097728fc6224afbedb670db307eedef07b08ae
Emre Yılmaz <email@emreyilmaz.me>
2010-07-07 13:58:37 +0300

    Custom exception types added. <thanks timu eren for suggestions for exception handling,>

--- src/fb.py
+++ src/fb.py
@@ -1,6 +1,33 @@
 # -*- coding: utf8 -*-
 
-import urllib
+import urllib, simplejson
+
+"""
+fb.py is a python client library for facebook api. it supports both *old rest api*
+and *new graph api*.
+
+you can find official documentation at facebook:
+    * old rest api: http://developers.facebook.com/docs/reference/rest/
+    * graph api: http://graph.facebook.com
+
+for the installation tips and usage examples, take a look to the readme.
+"""
+
+class GraphApiException(Exception):
+    """
+    custom exception class for graph api response errors.
+    """
+    def __init__(self, type, message):
+        Exception.__init__(self, message)
+        self.type = type
+        
+class RestApiException(Exception):
+    """
+    custom exception class for rest api response errors.
+    """
+    def __init__(self, error_code, message):
+        Exception.__init__(self, message)
+        self.error_code = error_code
 
 class RestApi(object):
     """
@@ -10,6 +37,13 @@ class RestApi(object):
 
     def __init__(self, token):
         self.auth_token = token
+        
+    """
+    handles api-response errors
+    """
+    def _handle_errors(self, api_response):
+        if isinstance(api_response, dict) and api_response["error_code"]:
+            raise RestApiException(api_response["error_code"], api_response["error_msg"])
         
     def get_object(self, call_method, **kargs):
         """
@@ -29,11 +63,12 @@ class RestApi(object):
             "format": "json"
         }
         query_string.update(kargs)
-        f = urllib.urlopen("https://api.facebook.com/method/%s?%s" % (call_method, urllib.urlencode(query_string)) )
-        return f.read()
- 
-    def _put_request(self):
-        pass
+        f = urllib.urlopen("https://api.facebook.com/method/%s?%s" % (call_method, urllib.urlencode(query_string)))
+        api_response = simplejson.loads(f.read())
+        self._handle_errors(api_response)
+        
+        return api_response
+
            
 class GraphApi(object):
     """
@@ -55,14 +90,24 @@ class GraphApi(object):
     """
     def put_object(self, request_path, post_data):
         return self._put_request(request_path, post_data)
-
+        
+    """
+    handles api-response errors
+    """
+    def _handle_errors(self, api_response):
+        if api_response["error"]:
+            raise GraphApiException(api_response["error"]["type"], api_response["error"]["message"])
+    
     def _get_request(self, request_path):
         """
         makes a HTTP (GET) request to the facebook graph api servers for given parameters. 
         (just for the information getter methods.)
         """
         f = urllib.urlopen("https://graph.facebook.com/%s?access_token=%s" % (request_path, self.auth_token))
-        return f.read()
+        api_response = simplejson.loads(f.read())
+        self._handle_errors(api_response)
+        
+        return api_response
     
     def _put_request(self, request_path, post_data):
         """
@@ -74,7 +119,10 @@ class GraphApi(object):
         })
         post_data = urllib.urlencode(post_data)
         f = urllib.urlopen("https://graph.facebook.com/%s" % request_path, post_data)
-        return f.read()
+        api_response = simplejson.loads(f.read())
+        self._handle_errors(api_response)
+        
+        return api_response
     
     def put_wall_post(self, user_alias, post_data):
         """
@@ -92,17 +140,22 @@ class GraphApi(object):
         * if you want to post to your running user's wall, just send user_alias parameter as "me".
         """
         return self._put_request("%s/feed" % user_alias, post_data)
-        
 
 class FBPY(object):
     """
     base FBPY object
     """
 
-    def __init__(self, token):
+    def __init__(self, token = None):
         self.auth_token = token
         self.graph_api_instance = None
         self.rest_api_instance  = None
+    
+    def set_token(self, token):
+        self.auth_token = token
+    
+    def is_authenticated(self):
+        return bool(self.auth_token)
         
     def graph(self):
         """
@@ -151,5 +204,7 @@ class FBPY(object):
             - session_key
         """
         return "https://www.facebook.com/logout.php?%s" % urllib.urlencode(params)
+
     
+
 
