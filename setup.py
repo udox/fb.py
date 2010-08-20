0e3e8d7f67cd4fb95fde48744f3913de275799d0
Emre Yılmaz <email@emreyilmaz.me>
2010-08-20 16:12:13 +0300

    new directory instructure

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='fbpy',
+      version='1.0',
+      description='python sdk for facebook apis',
+      author='Emre Yilmaz',
+      author_email='mail@emreyilmaz.me',
+      url='http://github.com/emre/fb.py',
+      packages = ['src/fbpy', 'src/fbpy/tools'],
+     )
+
