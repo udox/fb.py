4ce18193b267a5d20fbd304a17f8c3b631967c45
Emre Yılmaz <email@emreyilmaz.me>
2010-08-20 17:32:35 +0300

    fixed tabs:

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup
 
 setup(name='fbpy',
-      version='1.0',
-      description='python sdk for facebook apis',
-      author='Emre Yilmaz',
-      author_email='mail@emreyilmaz.me',
-      url='http://github.com/egnity/fb.py',
+    version='1.0',
+    description='python sdk for facebook apis',
+    author='Emre Yilmaz',
+    author_email='mail@emreyilmaz.me',
+    url='http://github.com/egnity/fb.py',
     package_dir={'': 'src'},
     packages = ["fbpy", "fbpy/tools",]
 )
