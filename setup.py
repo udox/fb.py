53f97b1d8da12643129cf2d8b53e90655be257b6
Emre Yılmaz <email@emreyilmaz.me>
2010-08-20 16:31:18 +0300

    fix for the pip install

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='fbpy',
       description='python sdk for facebook apis',
       author='Emre Yilmaz',
       author_email='mail@emreyilmaz.me',
-      url='http://github.com/emre/fb.py',
-      packages = ['src/fbpy', 'src/fbpy/tools'],
+      url='http://github.com/egnity/fb.py',
+      package_dir={'': 'src'},
      )
 
